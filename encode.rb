b48a57ddad55d9aa9b30439bc21cbc6ac7fabbcd
DuckOfDoom <horaxlfa@gmail.com>
2015-07-16 23:31:10 +0300

    Some encoded stuff

--- encode.rb
+++ encode.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 require 'json'
 
-@fields = ["id", "name", "type", "faction", "race", "rarity", "cost", "attack", "durability", "health", "text", "flavor", "playerClass", "mechanics"]
+@fields = ["id", "name", "type", "text", "attack", "health", "cost", "durability", "playerClass", "mechanics", "race",  "rarity", "faction",  "flavor"]
 
 def encode(hash)
     sets = hash.each{|h| h[0] }
@@ -10,10 +10,12 @@ def encode(hash)
     sets.each {|s| cards.concat(s[1]) }
     puts "Found #{cards.size} cards"
 
-    cards.map{ |c| @fields.map { |f| c[f] }.join("|")}
+    encoded = cards.map{ |c| @fields.map { |f| c[f] }.join("|").gsub("\n", "") }
+    shuffled = encoded.concat(encoded).concat(encoded).concat(encoded).shuffle
+
+    shuffled.join("\n\n\n")
 end
 
-file = File.open(ARGV[0], 'r:UTF-8', &:read);
+file = File.open(ARGV[0], 'r:UTF-8')
 output = encode(JSON.parse(file.read))
-
-#File.open(ARGV[1], 'w:UTF-8').write(output)
+File.open(ARGV[1], 'w:UTF-8').write(output)
